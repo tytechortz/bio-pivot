9eb7cb3c3df5817299ac6bf1b6dcc9b336efeb13
James Swank <jmswank7@gmail.com>
2021-08-11 06:44:05 -0600

    rework, table works

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output, State
+import dash_table
 import plotly.express as px
 import plotly.graph_objs as go
 import pandas as pd
@@ -10,18 +11,32 @@ from datetime import datetime as dt
 import dash_bootstrap_components as dbc
 from flask_sqlalchemy import SQLAlchemy
 from flask import Flask
+import os
 
 
-# from data import df_revenue, df_pc, df_rev
 server = Flask(__name__)
 app = dash.Dash(__name__)
 # app.config['suppress_callback_exceptions']=True
 app.config.suppress_callback_exceptions = True
 app.server.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-app.server.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://postgres:postgres@localhost/sites"
+app.server.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://postgres:1234@localhost/sites"
 
 db = SQLAlchemy(app.server)
+
+class Site(db.Model):
+    __tablename__ = 'sites'
+
+    site_name = db.Column(db.String(50), nullable=False, primary_key=True)
+    lat = db.Column(db.Integer, nullable=False)
+    lon = db.Column(db.Integer, nullable=False)
+
+    def __init__(self, site_name, lat, lon):
+        self.Site_name = site_name
+        self.Lat = lat
+        self.Lon = lon
+
+
 
 def get_emptyrow(h='15px'):
     """This returns an empty row of a defined height"""
@@ -46,14 +61,14 @@ def get_header(p= 'homepage'):
             style = {'padding-top' : '1%'}
         ),
         # get_emptyrow(),
-        html.Div([
-                dcc.Link(
-                    html.H2(children='Home', style={'color':'black'}),
-                    href='/homepage'
-                )
-            ],
-                style = {'padding-top' : '2.5%'}
-            ),
+        # html.Div([
+        #         dcc.Link(
+        #             html.H2(children='Home', style={'color':'black'}),
+        #             href='/homepage'
+        #         )
+        #     ],
+        #         style = {'padding-top' : '2.5%'}
+        #     ),
     ],
     className = 'row',
     style = {'height' : '4%'}
@@ -70,10 +85,16 @@ app.layout = html.Div([
             ),
             html.Div([
                 dcc.Input(
-                    id='add-site',
-                )
+                    id='adding-rows-name',
+                    placeholder='Enter a column name...',
+                    value='',
+                    style={'padding': 10}
+                ),
+                html.Button('Add Row', id='editing-rows-button', n_clicks=0),
+                html.Button('Save to PostgreSQL', id='save_to_postgres', n_clicks=0),
+                html.Button('Add Column', id='adding-columns-button', n_clicks=0),
             ],
-                className='col-2'
+                className='col-6'
             ),
             
         ],
@@ -92,6 +113,7 @@ app.layout = html.Div([
         ],
             className='row'
         ),
+        html.Div(id='postgres_datatable'),
         html.Div([
             html.Div([
                 dcc.Upload(
@@ -125,8 +147,77 @@ app.layout = html.Div([
         ],
             className='row'
         ),
+        dcc.Interval(id='interval_pg', interval=86400000*7, n_intervals=0),  # activated once/week or when page refreshed
+        html.Div(id='placeholder', children=[]),
+        dcc.Store(id="store", data=0),
+        dcc.Interval(id='interval', interval=1000),
     ])
+@app.callback(
+    Output('site-map', 'figure'),
+    Input('editing-rows-button', 'n_clicks'))
+def update_site_map(n):
+    print(n)
+    sites = pd.read_csv("sites.csv")
+    print(sites)
+    data = [dict(
+        lat = sites['lat'],
+        lon = sites['lon'],
+        type = 'scattermapbox',
+        marker = dict(size=7)
+    )]
 
+    layout = dict(
+        mapbox = dict(
+            
+        center = dict(lat=39, lon=-105.5),
+        accesstoken = os.environ.get("mapbox_token"),
+        zoom = 6,
+        style = 'light',
+        ),
+        hovermode = 'closest',
+        height = 500,
+        margin = dict(r=0, l=0, t=0, b=0),
+        clickmode = 'event+select'
+    )
+    fig = dict(data=data, layout=layout)
+    return fig
+
+@app.callback(Output('postgres_datatable', 'children'),
+              [Input('interval_pg', 'n_intervals')])
+def populate_datatable(n_intervals):
+    df = pd.read_sql_table('sites', con=db.engine)
+    return [
+        dash_table.DataTable(
+            id='our-table',
+            columns=[{
+                         'name': str(x),
+                         'id': str(x),
+                         'deletable': False,
+                     } if x == 'Sales' or x == 'Phone'
+                     else {
+                'name': str(x),
+                'id': str(x),
+                'deletable': True,
+            }
+                     for x in df.columns],
+            data=df.to_dict('records'),
+            editable=True,
+            row_deletable=True,
+            filter_action="native",
+            sort_action="native",  # give user capability to sort columns
+            sort_mode="single",  # sort across 'multi' or 'single' columns
+            page_action='none',  # render all of the data at once. No paging.
+            style_table={'height': '300px', 'overflowY': 'auto'},
+            style_cell={'textAlign': 'left', 'minWidth': '100px', 'width': '100px', 'maxWidth': '100px'},
+            style_cell_conditional=[
+                {
+                    'if': {'column_id': c},
+                    'textAlign': 'right'
+                } for c in ['Price', 'Sales']
+            ]
+
+        ),
+    ]
 
 
 if __name__ == '__main__':
