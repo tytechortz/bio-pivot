5fc047e10a619cb35456204419bce933068a582f
James Swank <jmswank7@gmail.com>
2021-08-11 11:42:11 -0600

    displays csv upload in table

--- app.py
+++ app.py
@@ -12,6 +12,9 @@ import dash_bootstrap_components as dbc
 from flask_sqlalchemy import SQLAlchemy
 from flask import Flask
 import os
+import base64
+import datetime
+import io
 
 
 server = Flask(__name__)
@@ -75,6 +78,43 @@ def get_header(p= 'homepage'):
     )
 
     return header
+
+def parse_contents(contents, filename, date):
+    content_type, content_string = contents.split(',')
+
+    decoded = base64.b64decode(content_string)
+    try:
+        if 'csv' in filename:
+            # Assume that the user uploaded a CSV file
+            df = pd.read_csv(
+                io.StringIO(decoded.decode('utf-8')))
+        elif 'xls' in filename:
+            # Assume that the user uploaded an excel file
+            df = pd.read_excel(io.BytesIO(decoded))
+    except Exception as e:
+        print(e)
+        return html.Div([
+            'There was an error processing this file.'
+        ])
+
+    return html.Div([
+        html.H5(filename),
+        html.H6(datetime.datetime.fromtimestamp(date)),
+
+        dash_table.DataTable(
+            data=df.to_dict('records'),
+            columns=[{'name': i, 'id': i} for i in df.columns]
+        ),
+
+        html.Hr(),  # horizontal line
+
+        # For debugging, display the raw contents provided by the web browser
+        html.Div('Raw Content'),
+        html.Pre(contents[0:200] + '...', style={
+            'whiteSpace': 'pre-wrap',
+            'wordBreak': 'break-all'
+        })
+    ])
 
 app.layout = html.Div([
         get_header(),
@@ -168,6 +208,19 @@ app.layout = html.Div([
         dcc.Store(id="store", data=0),
         dcc.Interval(id='interval', interval=1000),
     ])
+
+@app.callback(Output('output-data-upload', 'children'),
+              Input('upload-data', 'contents'),
+              State('upload-data', 'filename'),
+              State('upload-data', 'last_modified'))
+def update_output(list_of_contents, list_of_names, list_of_dates):
+    if list_of_contents is not None:
+        children = [
+            parse_contents(c, n, d) for c, n, d in
+            zip(list_of_contents, list_of_names, list_of_dates)]
+        return children
+
+
 @app.callback(
     Output('site-map', 'figure'),
     Input('interval_pg', 'n_intervals'))
@@ -216,7 +269,7 @@ def populate_datatable(n_intervals):
                      for x in df.columns],
             data=df.to_dict('records'),
             editable=True,
-            row_deletable=False,
+            row_deletable=True,
             filter_action="native",
             sort_action="native",  # give user capability to sort columns
             sort_mode="single",  # sort across 'multi' or 'single' columns
