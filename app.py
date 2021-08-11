c5707f980aa65e987e6e87d61d7644b0b4721068
James Swank <jmswank7@gmail.com>
2021-08-11 08:28:01 -0600

    map linked to db

--- app.py
+++ app.py
@@ -78,6 +78,21 @@ def get_header(p= 'homepage'):
 
 app.layout = html.Div([
         get_header(),
+        
+        html.Div([
+            html.Div([
+            ],
+                className='col-1'
+            ),
+            html.Div([
+                dcc.Graph('site-map')
+            ],
+                className='col-10'
+            ),
+        ],
+            className='row'
+        ),
+        get_emptyrow(),
         html.Div([
             html.Div([
             ],
@@ -100,21 +115,12 @@ app.layout = html.Div([
         ],
             className='row'
         ),
+        get_emptyrow(),
         html.Div([
             html.Div([
             ],
                 className='col-1'
             ),
-            html.Div([
-                dcc.Graph('site-map')
-            ],
-                className='col-10'
-            ),
-        ],
-            className='row'
-        ),
-        get_emptyrow(),
-        html.Div([
             html.Div([
                 html.Div(id='postgres_datatable'),
             ],
@@ -164,10 +170,11 @@ app.layout = html.Div([
     ])
 @app.callback(
     Output('site-map', 'figure'),
-    Input('editing-rows-button', 'n_clicks'))
-def update_site_map(n):
-    print(n)
-    sites = pd.read_csv("sites.csv")
+    Input('interval_pg', 'n_intervals'))
+def update_site_map(n_intervals):
+    # print(n)
+    # sites = pd.read_csv("sites.csv")
+    sites = pd.read_sql_table('sites', con=db.engine)
     print(sites)
     data = [dict(
         lat = sites['lat'],
@@ -228,6 +235,61 @@ def populate_datatable(n_intervals):
 
         ),
     ]
+
+@app.callback(
+    Output('our-table', 'columns'),
+    [Input('adding-columns-button', 'n_clicks')],
+    [State('adding-rows-name', 'value'),
+     State('our-table', 'columns')],
+    prevent_initial_call=True)
+def add_columns(n_clicks, value, existing_columns):
+    if n_clicks > 0:
+        existing_columns.append({
+            'name': value, 'id': value,
+            'renamable': True, 'deletable': True
+        })
+    return existing_columns
+
+
+@app.callback(
+    Output('our-table', 'data'),
+    [Input('editing-rows-button', 'n_clicks')],
+    [State('our-table', 'data'),
+     State('our-table', 'columns')],
+    prevent_initial_call=True)
+def add_row(n_clicks, rows, columns):
+    if n_clicks > 0:
+        rows.append({c['id']: '' for c in columns})
+    return rows
+
+@app.callback(
+    [Output('placeholder', 'children'),
+     Output("store", "data")],
+    [Input('save_to_postgres', 'n_clicks'),
+     Input("interval", "n_intervals")],
+    [State('our-table', 'data'),
+     State('store', 'data')],
+    prevent_initial_call=True)
+def df_to_csv(n_clicks, n_intervals, dataset, s):
+    output = html.Plaintext("The data has been saved to your PostgreSQL database.",
+                            style={'color': 'green', 'font-weight': 'bold', 'font-size': 'large'})
+    no_output = html.Plaintext("", style={'margin': "0px"})
+
+    input_triggered = dash.callback_context.triggered[0]["prop_id"].split(".")[0]
+
+    if input_triggered == "save_to_postgres":
+        s = 6
+        pg = pd.DataFrame(dataset)
+        pg.to_sql("sites", con=db.engine, if_exists='replace', index=False)
+        return output, s
+    elif input_triggered == 'interval' and s > 0:
+        s = s - 1
+        if s > 0:
+            return output, s
+        else:
+            return no_output, s
+    elif s == 0:
+        return no_output, s
 
 
 if __name__ == '__main__':
